9700c017fce6d3ee4e297a4ea1cf4dff6b2a4a15
Jan-Rajnak <janrajnak333@gmail.com>
2025-01-18 17:02:30 +0100

    simplified the navigation even more

--- js/navigation.js
+++ js/navigation.js
@@ -1,15 +1,17 @@
 // File: js/navigation.js
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    fillHeader();
+
     const navItems = [
-        { key: 'nav.home', href: 'home.html', text: 'Home' },
-        { key: 'nav.about', href: 'about.html', text: 'About' },
-        { key: 'nav.repertoire', href: 'repertoire.html', text: 'Repertoire' },
-        { key: 'nav.members', href: 'members.html', text: 'Members' },
-        { key: 'nav.recordings', href: 'recordings.html', text: 'Recordings' },
-        { key: 'nav.gallery', href: 'gallery.html', text: 'Gallery' },
-        { key: 'nav.actualities', href: 'actualities.html', text: 'Actualities' },
-        { key: 'nav.contact', href: 'contact.html', text: 'Contact' }
+        {key: 'nav.home', href: 'home.html', text: 'Home'},
+        {key: 'nav.about', href: 'about.html', text: 'About'},
+        {key: 'nav.repertoire', href: 'repertoire.html', text: 'Repertoire'},
+        {key: 'nav.members', href: 'members.html', text: 'Members'},
+        {key: 'nav.recordings', href: 'recordings.html', text: 'Recordings'},
+        {key: 'nav.gallery', href: 'gallery.html', text: 'Gallery'},
+        {key: 'nav.actualities', href: 'actualities.html', text: 'Actualities'},
+        {key: 'nav.contact', href: 'contact.html', text: 'Contact'}
     ];
 
     const navContainer = document.querySelector('.header-nav');
@@ -30,6 +32,18 @@ document.addEventListener('DOMContentLoaded', function() {
         activeLink.classList.add('active');
     }
 
-    translate();
-    document.body.classList.remove('hidden');
+    translate().then(() => {
+        document.body.classList.remove('hidden');
+    });
 });
+
+function fillHeader() {
+    const header = document.querySelector('.header');
+    header.innerHTML = `
+        <a class="header-logo" href="home.html">
+            <img alt="logo" src="../media/IMGs/logo/headerLogo.png"/>
+        </a>
+        <div class="header-title">Stalegria</div>
+        <nav class="header-nav"></nav>
+    `;
+}
