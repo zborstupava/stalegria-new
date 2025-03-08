e594be869b28340b2c472b8e091629df900677ab
Jan-Rajnak <janrajnak333@gmail.com>
2025-03-08 20:36:56 +0100

    started work on responsive design for mobile layout

--- js/navigation.js
+++ js/navigation.js
@@ -15,6 +15,8 @@ document.addEventListener('DOMContentLoaded', function () {
     ];
 
     const navContainer = document.querySelector('.header-nav');
+    const mobileNavPanel = document.querySelector('.mobile-nav-panel');
+    mobileNavPanel.innerHTML = '<button class="close-mobile-nav-panel">X</button>';
 
     navItems.forEach(item => {
         const navLink = document.createElement('a');
@@ -23,6 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
         navLink.href = item.href;
         navLink.textContent = item.text;
         navContainer.appendChild(navLink);
+        mobileNavPanel.appendChild(navLink.cloneNode(true));
     });
 
     // Set the active link
@@ -35,15 +38,30 @@ document.addEventListener('DOMContentLoaded', function () {
     translate().then(() => {
         document.body.classList.remove('hidden');
     });
+
+    // Toggle mobile navigation panel
+    const mobileNavButton = document.querySelector('.mobile-nav-button');
+    mobileNavButton.addEventListener('click', function () {
+        mobileNavPanel.classList.toggle('open');
+    });
+
+    // Close mobile navigation panel
+    const closeMobileNavButton = document.querySelector('.close-mobile-nav-panel');
+    closeMobileNavButton.addEventListener('click', function () {
+        mobileNavPanel.classList.remove('open');
+    });
 });
 
 function fillHeader() {
     const header = document.querySelector('.header');
     header.innerHTML = `
-        <a class="header-logo" href="home.html">
+        <a class="header-logo" href="index.html">
             <img alt="logo" src="/stalegria-new/media/IMGs/logo/headerLogo.png"/>
         </a>
         <div class="header-title">Stalegria</div>
+        <div class="header-gap"></div>
+        <button class="mobile-nav-button">☰</button>
         <nav class="header-nav"></nav>
+        <div class="mobile-nav-panel"></div>
     `;
 }
